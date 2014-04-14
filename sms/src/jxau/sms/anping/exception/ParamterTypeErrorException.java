73dd049a6a38fa1e98c6c4014ddb8bd7f830ac4b
anping <974984076@qq.com>
2014-04-14 22:59:56 +0800

    add yibao

---
+++ sms/src/jxau/sms/anping/exception/ParamterTypeErrorException.java
@@ -0,0 +1,13 @@
+package jxau.sms.anping.exception;
+
+public class ParamterTypeErrorException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ParamterTypeErrorException(String mesage){
+		super(mesage);
+	}
+
+}

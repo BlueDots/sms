1a1f7ada9b396f05ea59e7591f9ab818a634393f
chengzhengqing <chzhq2011@126.com>
2014-04-16 21:03:31 +0800

    add

--- sms/src/jxau/sms/qing/login/service/LoginServiceTest.java
+++ sms/src/jxau/sms/qing/login/service/LoginServiceTest.java
@@ -1,15 +1,20 @@
 package jxau.sms.qing.login.service;
 
 import static org.junit.Assert.*;
+import jxau.sms.globalService.GlobalServiceInterface;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LoginServiceTest {
 
 	@Test
 	public void test() {
-		new LoginService().selectStudent("20111112", "mm");
-		//new LoginService().selectTeacher("0000", "ll");
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		LoginService loginService = (LoginService) context.getBean("loginService");
+		loginService.selectStudent("20111112", "mm");
+		loginService.selectTeacher("0000", "mm"); 
 	}
 
 }
--- sms/src/jxau/sms/qing/po/Student.java
+++ sms/src/jxau/sms/qing/po/Student.java
@@ -2,19 +2,81 @@ package jxau.sms.qing.po;
 
 public class Student {
 	private String studentNo;
-	private String stuPassword;
+	private String studentName;
+	private String className;
+	private String major;
+	private String college;
+	private int sex;
+	private String telephone;
+	private String email;
+	private int emailOnly;
+	private String bankCard;
+	private int stuState;
 	public String getStudentNo() {
 		return studentNo;
 	}
 	public void setStudentNo(String studentNo) {
 		this.studentNo = studentNo;
 	}
-	public String getStuPassword() {
-		return stuPassword;
+	public String getStudentName() {
+		return studentName;
 	}
-	public void setStuPassword(String stuPassword) {
-		this.stuPassword = stuPassword;
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
 	}
-	
-	
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getEmailOnly() {
+		return emailOnly;
+	}
+	public void setEmailOnly(int emailOnly) {
+		this.emailOnly = emailOnly;
+	}
+	public String getBankCard() {
+		return bankCard;
+	}
+	public void setBankCard(String bankCard) {
+		this.bankCard = bankCard;
+	}
+	public int getStuState() {
+		return stuState;
+	}
+	public void setStuState(int stuState) {
+		this.stuState = stuState;
+	}	
+
 }

db3f3b0f2698ce3bc62135c2869d9118208626fe
yixiangli <394244674@qq.com>
2014-04-18 11:05:44 +0800

    lyx23

---
+++ sms/src/jxau/sms/anping/action/DisplayCollegeAction.java
@@ -0,0 +1,46 @@
+package jxau.sms.anping.action;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import jxau.sms.anping.po.DepInfo;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DisplayCollegeAction extends ActionSupport{
+
+	/**
+	 * 需要从session中拿出数据，显示的是它最大的角色，并拿出相应的数据
+	 * 如最大的角色是班主任，则显示的应该是所教院，所交方向，所交班
+	 * 如果最大的角色是院级工作人员是拿出所在院，所在方向，所在班
+	 * 如果是校级，则全部显示
+	 * anping
+	 * TODO
+	 * 上午9:34:35
+	 * @return
+	 */
+	public String showCollege(){
+		for(int i =0 ;i<10;i++){
+			DepInfo  depInfo  = new DepInfo();
+			depInfo.setDepNum(1);
+			depInfo.setDepartInfo("aaa");
+			depInfo.setDepartNo("1");
+			depInfo.setDepartment("aaaa");
+			depInfos.add(depInfo);
+		}
+		return SUCCESS;
+	}
+	
+	
+	
+	public List<DepInfo> getDepInfos() {
+		return depInfos;
+	}
+	private List<DepInfo>  depInfos=new LinkedList<DepInfo>() ;
+	private Map<String,Object> session  = ActionContext.getContext().getSession();
+	private Map<String,Object> params = ActionContext.getContext().getParameters();
+	private static final long serialVersionUID = 1L;
+	
+}
